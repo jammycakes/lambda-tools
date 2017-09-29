dcc506d955bff753c256d729d8c6bc60788ac43e
James McKay <code@jamesmckay.net>
2017-09-29 11:52:28 +0100

    Basic skeleton for package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lambda_tools',
+    version='0.1',
+    description='A toolkit for creating and deploying Python code to AWS Lambda',
+    url='https://github.com/jammycakes/lambda-tools',
+    author='James McKay',
+    author_email='code@jamesmckay.net',
+    license='MIT',
+    packages=[],
+    zip_safe=False
+)
