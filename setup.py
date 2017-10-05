50c097a5a6f8a45875c7baacbf1f52ba4c9d1900
James McKay <code@jamesmckay.net>
2017-10-05 10:51:47 +0100

    Some final preparations for packaging

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ pip_requirements = [str(r.req) for r in requirements]
 
 setup(
     name='lambda_tools',
+    packages=['lambda_tools'],
     version='0.1',
     description='A toolkit for creating and deploying Python code to AWS Lambda',
-    url='https://github.com/jammycakes/lambda-tools',
     author='James McKay',
     author_email='code@jamesmckay.net',
+    keywords=['aws-lambda', 'aws'],
+    url='https://github.com/jammycakes/lambda-tools',
+    download_url = 'https://github.com/jammycakes/lambda-tools/archive/0.1.tar.gz',
     license='MIT',
-    packages=['lambda_tools'],
     install_requires=pip_requirements,
     zip_safe=False,
     entry_points={
